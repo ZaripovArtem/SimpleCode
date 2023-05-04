a047f8bb9f6c480d163f17428c18d23fdb41c91d
Артем <artemzaripov2002@gmail.com>
2023-05-04 17:16:06 +0300

    Добавьте файлы проекта.

---
+++ SimpleCode/Program.cs
@@ -0,0 +1,8 @@
+﻿using SimpleCode;
+
+string[] A = { "unlock.microvirus.md", "visitwar.com", "visitwar.de", "fruonline.co.uk", "australia.open.com", "credit.card.us" };
+string[] B = { "microvirus.md", "visitwar.de", "piratebay.co.uk", "list.stolen.credit.card.us" };
+var result = Solution.solution(A, B);
+
+foreach (var item in result)
+    Console.WriteLine(item);
---
+++ SimpleCode/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace SimpleCode
+{
+    public class Solution
+    {
+        public static int[] solution(string[] A, string[] B)
+        {
+            List<int> res = new();
+            int check = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < B.Length; j++)
+                {
+                    if (A[i].EndsWith(B[j]))
+                    {
+                        check++;
+                    }
+                }
+                if (check == 0)
+                {
+                    res.Add(i);
+                }
+                else { check = 0; }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
